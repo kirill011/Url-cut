0b873ed6b45a74e92e5a2c494e8fe2c56615a62a
kirill <kirill.petrov.2001@list.ru>
2022-09-25 15:03:27 +0300

    v1.1
    Signed-off-by: kirill <kirill.petrov.2001@list.ru>

--- postgres.sql
+++ postgres.sql
@@ -1,11 +1,10 @@
-CREATE DATABASE Url-cut
 
 
 CREATE TABLE IF NOT EXISTS public.url
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     link text  NOT NULL,
-    short character varying(10) NOT NULL,
+    short character varying(40) NOT NULL,
     CONSTRAINT url_pkey PRIMARY KEY (id),
     CONSTRAINT unique_short UNIQUE (short)
 )
